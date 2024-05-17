4f8f737199dcb88c518f12c259d0f04fb5be5555
SaulVas <saul.vassallo.22@um.edu.mt>
2024-05-17 16:34:02 +0200

    wall follower state at end of friday

---
+++ wall_follower/InfraRedSensor.h
@@ -0,0 +1,29 @@
+#ifndef _InfraRedSensor_H_
+#define _InfraRedSensor_H_
+
+#include <arduino.h>
+
+/*Infrared Sensor*/
+class InfraRedSensor {
+    private:
+    // Pins for the Infrared Sensors
+    #define LEFT_IRS_PIN A2
+    #define MIDDLE_IRS_PIN A1
+    #define RIGHT_IRS_PIN A0
+
+    int get_analogue_value(int pin);
+
+public: 
+    # define THRESHOLD 600
+    # define BLACK_LOWER 800
+    # define BLACK_UPPER 900
+    # define GRAY_LOWER 300
+    # define GRAY_UPPER 800
+
+    void init(void);
+    int get_left(void);
+    int get_middle(void);
+    int get_right(void);
+};
+
+#endif
--- wall_follower/wall_follower.ino
+++ wall_follower/wall_follower.ino
@@ -2,21 +2,25 @@
 #include "Motor.h"
 #include "UltraSonicSensor.h"
 #include "ServoRotate.h"
+#include "InfraRedSensor.h"
 
 Motor motor;
 UltraSonicSensor ultrasonic;
 ServoRotate servo;
+InfraRedSensor irs;
 unsigned long start_time;
-const int targetDistance = 10; // Target distance from the wall in cm
+const int targetDistance = 12; // Target distance from the wall in cm
 const float proportionalGain = 5.5; // Proportional gain for correction
 int baseSpeed = 100;
+bool rightWall = true;
+bool foundEnd = false;
 
 
 void setup() {
   motor.init();
   ultrasonic.init();
   servo.init();
-  Serial.begin(9600);
+  irs.init();
   servo.rotateTo(0);
   delay(1000);
   start_time = millis();
@@ -28,59 +32,170 @@ void setup() {
 
 void loop() {
   unsigned long current_time = millis();
+  int left = irs.get_left();
+  int right = irs.get_right();
+  int middle = irs.get_middle();
 
-  if (current_time - start_time > 450) {
-    motor.move(STOP, 0);
-    servo.rotateTo(90);
-    delay(350);
-    ultrasonic.send_pulse();
-    int frontDistance = ultrasonic.get_distance();
-    
-    if (frontDistance < 10) {
-      motor.move(LEFT, 200);
-      delay(270);
+  if (!foundEnd) {
+    if ((left > BLACK_LOWER && left < BLACK_UPPER) && (middle > BLACK_LOWER && middle < BLACK_UPPER) && (right > BLACK_LOWER && right < BLACK_UPPER)){
+      foundEnd = true;
+      motor.move(FORWARD, 100);
+      delay(350);
+      motor.move(STOP, 0);
+      delay(1000);
+      servo.rotateTo(90);
+      delay(350);
+      ultrasonic.send_pulse();
+      int frontDistance = ultrasonic.get_distance();
+
+      if (frontDistance < 20) {
+        servo.rotateTo(180);
+        delay(350);
+        ultrasonic.send_pulse();
+        int leftDistance = ultrasonic.get_distance();
+        if (leftDistance < 20) {
+          motor.move(LEFT, 200);
+          delay(500);
+          motor.move(STOP, 0);
+          delay(50);
+        }
+        else {
+          motor.move(LEFT, 200);
+          delay(250);
+          motor.move(STOP, 0);
+          delay(50);
+        }
+      }
+    }
+  }
+  else if (foundEnd) {
+    if ((left > GRAY_LOWER && left < GRAY_UPPER) && (middle > GRAY_LOWER && middle < GRAY_UPPER) && (right > GRAY_LOWER && right < GRAY_UPPER)){
+        motor.move(FORWARD, baseSpeed);
+        delay(400);
+        rightWall = false;
+        motor.move(STOP, 0);
+        delay(1000);
+        motor.move(LEFT, 200);
+        delay(500);
+        motor.move(STOP, 0);
+        delay(50);
+  }
+}
+  
+  if (rightWall) {  
+    if (current_time - start_time > 400) {
+      motor.move(STOP, 0);
+      servo.rotateTo(90);
+      delay(350);
+      ultrasonic.send_pulse();
+      int frontDistance = ultrasonic.get_distance();
+      
+      if (frontDistance < 10) {
+          motor.move(LEFT, 200);
+          delay(270);
+          motor.move(STOP, 0);
+          delay(50);
+      }
+
       motor.move(STOP, 0);
       delay(50);
-    }
+      ultrasonic.send_pulse();
+      frontDistance = ultrasonic.get_distance();
+      if (frontDistance < 12) {
+          motor.move(LEFT, 200);
+          delay(270);
+          motor.move(STOP, 0);
+          delay(50);
+      }
 
+      servo.rotateTo(0);
+      delay(350);
+      start_time = millis();
+    }
     ultrasonic.send_pulse();
-    frontDistance = ultrasonic.get_distance();
-    if (frontDistance < 12) {
-      motor.move(LEFT, 200);
-      delay(225);
-      motor.move(STOP, 0);
-      delay(50);
+    int currentDistance = ultrasonic.get_distance();
+
+    // Calculate error with saturation
+    int error;
+    if (currentDistance > 25) {
+      error = 25 - targetDistance; // Cap the error to the value at 25 cm
+    } else {
+      error = currentDistance - targetDistance;
     }
 
-    servo.rotateTo(0);
-    delay(350);
-    start_time = millis();
+    // Calculate speed adjustment
+    int speedAdjustment = proportionalGain * error;
+
+    // Set motor speeds
+    int rightMotorSpeed = baseSpeed - speedAdjustment;
+    int leftMotorSpeed = baseSpeed + speedAdjustment;
+
+    // Ensure speeds remain within 0-255
+    rightMotorSpeed = constrain(rightMotorSpeed, 0, 150);
+    leftMotorSpeed = constrain(leftMotorSpeed, 0, 150);
+
+    // Move robot
+    motor.motor_control(DIRECTION_FRONT, rightMotorSpeed, DIRECTION_FRONT, leftMotorSpeed, CONTROL_ENABLE);
   }
-  ultrasonic.send_pulse();
-  int currentDistance = ultrasonic.get_distance();
+  else if (!rightWall) {
+    if (!rightWall){
+      if ((left > BLACK_LOWER && left < BLACK_UPPER) && (middle > BLACK_LOWER && middle < BLACK_UPPER) && (right > BLACK_LOWER && right < BLACK_UPPER)){
+        motor.move(STOP, 0);
+        delay(300000);
+      }
+    }
+    if (current_time - start_time > 400) {
+      motor.move(STOP, 0);
+      servo.rotateTo(90);
+      delay(350);
+      ultrasonic.send_pulse();
+      int frontDistance = ultrasonic.get_distance();
+      
+      if (frontDistance < 10) {
+        motor.move(RIGHT, 200);
+        delay(270);
+        motor.move(STOP, 0);
+        delay(50);
+      }
 
-  // Calculate error with saturation
-  int error;
-  if (currentDistance > 25) {
-    error = 25 - targetDistance; // Cap the error to the value at 25 cm
-  } else {
-    error = currentDistance - targetDistance;
+      ultrasonic.send_pulse();
+      frontDistance = ultrasonic.get_distance();
+      if (frontDistance < 12) {
+        motor.move(RIGHT, 200);
+        delay(225);
+        motor.move(STOP, 0);
+        delay(50);
+      }
+
+      servo.rotateTo(180);
+      delay(350);
+      start_time = millis();
+    }
+    ultrasonic.send_pulse();
+    int currentDistance = ultrasonic.get_distance();
+
+    // Calculate error with saturation
+    int error;
+    if (currentDistance > 25) {
+      error = 25 - targetDistance; // Cap the error to the value at 25 cm
+    } else {
+      error = currentDistance - targetDistance;
+    }
+
+    // Calculate speed adjustment
+    int speedAdjustment = proportionalGain * error;
+
+    // Set motor speeds
+    int rightMotorSpeed = baseSpeed + speedAdjustment;
+    int leftMotorSpeed = baseSpeed - speedAdjustment;
+
+    // Ensure speeds remain within 0-255
+    rightMotorSpeed = constrain(rightMotorSpeed, 0, 150);
+    leftMotorSpeed = constrain(leftMotorSpeed, 0, 150);
+
+    // Move robot
+    motor.motor_control(DIRECTION_FRONT, rightMotorSpeed, DIRECTION_FRONT, leftMotorSpeed, CONTROL_ENABLE);
   }
-
-  // Calculate speed adjustment
-  int speedAdjustment = proportionalGain * error;
-
-  // Set motor speeds
-  int rightMotorSpeed = baseSpeed - speedAdjustment;
-  int leftMotorSpeed = baseSpeed + speedAdjustment;
-
-  // Ensure speeds remain within 0-255
-  rightMotorSpeed = constrain(rightMotorSpeed, 0, 150);
-  leftMotorSpeed = constrain(leftMotorSpeed, 0, 150);
-
-  // Move robot
-  motor.motor_control(DIRECTION_FRONT, rightMotorSpeed, DIRECTION_FRONT, leftMotorSpeed, CONTROL_ENABLE);
-
   // Delay to allow for movement
   delay(75);
    
